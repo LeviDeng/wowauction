e2a10dced9acb790ea9d8407c4312bf060a7064e
levi <levi@exabeam.com>
2017-06-20 18:55:24 +0800

    添加统计函数

--- getdata.py
+++ getdata.py
@@ -1,7 +1,7 @@
 #coding:utf8
 import requests
 import pymongo
-from time import sleep
+import time
 
 #REGION=['us','eu','tw','kr']
 TIME_SLEEP=600
@@ -14,6 +14,7 @@ class wowAuction():
     def __init__(self):
         self.baseUrl="https://%s.api.battle.net/wow/auction/data/"%REGION
         self.auctionUrl=self.baseUrl+"%s?locale=%s&apikey=%s"%(REALM,LOCALE,APIKEY)
+        self.datetime=time.strftime("%Y%m%d%H",time.localtime())
         #print self.auctionUrl
 
     def getDataUrl(self):
@@ -26,12 +27,26 @@ class wowAuction():
         data_url=self.getDataUrl()
         res=requests.get(data_url)
         data=res.json()
-        coll.update_one({"realms.name":REALM},{"$set":data},upsert=True)
-        #coll.update(data,upsert=True)
+        data['time'] = self.datetime
+        coll.insert_one(data)
+        self.itemlist=coll.distinct("auctions.item")
         con.close()
+
+    def analyse(self):
+        con=pymongo.MongoClient('localhost',27017)
+        coll1=con['wowAuction'][REGION]
+        coll2=con['wowAuction'][REGION+"_stat"]
+        data1=coll1.find({"time":self.datetime})[0]['auctions']
+        data2={"time":self.datetime}
+        for i in self.itemlist:
+            data2[str(i)]=[]
+            for d in data1:
+                if d['item']==i:
+                    data2[str(i)].append(d['buyout'])
+            data2[str(i)]=sorted(data2[str(i)])
+        coll2.insert_one(data2)
 
 if __name__=='__main__':
     wa=wowAuction()
-    while True:
-        wa.getData()
-        sleep(TIME_SLEEP)
+    wa.getData()
+    wa.analyse()
